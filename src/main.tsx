78cce37f660f692b6f74c06f541c4f8aaff75cde
wtfisquantum <92854101+wtfisquantum@users.noreply.github.com>
2026-08-02 19:41:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import NewTab from './newtab.tsx'
+// import NewTab from './newtab.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NewTab />
+    <App />
   </StrictMode>,
 )
